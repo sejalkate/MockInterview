823ae7c8ad3eac9b733377db364e616016dc2243
sejalkate <114280907+sejalkate@users.noreply.github.com>
2025-07-23 15:37:06 +0530

    Create Person.java

---
+++ KiranAcademy/Inheritance/Person.java
@@ -0,0 +1,12 @@
+package com.interview;
+
+public class Person
+{
+	String name;
+    String address;
+    public Person(String name,String address) 
+    {
+        this.name=name;
+        this.address=address;
+    }
+}
